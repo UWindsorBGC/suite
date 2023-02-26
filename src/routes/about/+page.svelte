b7456283696d9938a01619f9f7614bf4f262af30
OneEyedKnight <36034467+OneEyedKnight@users.noreply.github.com>
2023-02-25 23:46:32 -0500

    put about page back

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,62 @@
+<section id="mission">
+    <h1>Our Mission</h1>
+    <!-- Always wrap large text. -->
+    <p>
+        The UWindsor Board Games Club is an organization formed to offer students the opportunity to expand their
+        strategic knowledge and critical thinking skills by providing a safe and fun environment away from the stresses
+        of academia for all game lovers whether it be board games, card games, or anything the mind can imagine.
+    </p>
+    <p>
+        Our aim is to make board gaming an accessible and enjoyable fun experience for everyone. To become the most
+        reliable place for organizing quality modern board gaming events, by being an accessible global meeting place
+        anybody can use, designed to meet the specific needs of the board game community in a user friendly and
+        convenient way.
+    </p>
+    <p>
+        We play for fun and for education! We aim to teach, promote and make available games to build community within
+        the club. We are dedicated to fair fun, inclusivity and good sportsmanship both in-game and in our interactions.
+    </p>
+    <p>
+        As such, we encourage people of all levels of familiarity and skill to participate in our events. We aim to
+        provide an environment that allows people to experience all types of games, emphasizing fun over competition.
+        Everyone is welcome to become a member, so invite your friends and come play some games!
+    </p>
+</section>
+
+<section id="team">
+    <h1>Meet the Team</h1>
+    <p>
+        Our club management consists of The President, Vice-President, Treasurer, Game Master, and Host. They are
+        elected by active members of the club to serve in the club's interest. Their roles in the club are defined in
+        our constitution. Members may have more than one role.
+    </p>
+    <h2>Executive Body</h2>
+
+    <h2>Hosts</h2>
+</section>
+
+<style>
+    #mission,
+    #team {
+        background-color: var(--element-bg);
+        color: white;
+        width: 50%;
+        margin: 0 auto;
+        padding: 2rem;
+        font-size: 18px;
+        border-radius: 1rem;
+        margin-bottom: 5em;
+    }
+
+    #mission h1 {
+        text-align: center;
+    }
+
+    #mission p {
+        text-indent: 40px;
+    }
+
+    #team h1 {
+        text-align: center;
+    }
+</style>
